d56f9cd723b5d9fd1f7e249f18b759ebdfff2ccc
Nicolas Lucien <86562548+Tech-FZ@users.noreply.github.com>
2022-06-19 09:49:58 +0200

    Add files via upload

---
+++ RandochooseMobile/RandochooseMobile/MainPage.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace RandochooseMobile
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        int count = 0;
+
+        private void Handle_Clicked(object sender, System.EventArgs e)
+        {
+            string selectedItemsStr = items.Text;
+            string[] selectedItems = selectedItemsStr.Split('\n');
+
+            Random rnd = new Random();
+            count = rnd.Next(0, selectedItems.Length);
+
+            chosenItem.Text = $"{selectedItems[count]} has been chosen.";
+        }
+
+        private async void Button_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new About());
+        }
+    }
+}
